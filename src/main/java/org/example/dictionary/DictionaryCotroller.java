fb32a6f8663027f0dcb88fe5844ee36be9bd17da
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-02-26 23:43:37 +0700

    dictionary

--- src/main/java/org/example/dictionary/DictionaryCotroller.java
+++ src/main/java/org/example/dictionary/DictionaryCotroller.java
@@ -1,2 +1,41 @@
-package org.example.dictionary;public class DictionaryCotroller {
+package org.example.dictionary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Controller
+
+public class DictionaryCotroller {
+    @GetMapping("/home")
+    public String showDictionary(){
+        return "home";
+    }
+    @PostMapping("/home")
+    public String simpleDictionary(@RequestParam("dictionary") String dictionary, Model model){
+        Map<String, String> stringMap = new HashMap<>();
+        stringMap.put("hello", "xin chào");
+        stringMap.put("book", "sách");
+        stringMap.put("student", "học sinh");
+        String tiengViet = stringMap.get(dictionary);
+        model.addAttribute("tiengViet", tiengViet);
+
+        boolean check = false;
+        for (Map.Entry<String, String> entry: stringMap.entrySet()){
+            if (entry.getKey().equalsIgnoreCase(tiengViet)){
+                model.addAttribute("result", entry.getValue());
+                check = true;
+            }
+        }
+        if (check){
+            model.addAttribute("result", "Không tìm thấy");
+        }
+        return "home";
+
+    }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -7,10 +7,22 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Ứng dụng Từ điển đơn giản</title>
+</head>
+<body>
+<h1>Ứng dụng từ điển!</h1>
+<form method="post">
+    <fieldset>
+        <legend>
+            Dictionary
+        </legend>
+        <label for="dictionary">Nhập từ cần dịch</label>
+        <input id="dictionary" name="dictionary" type="text" required>
+        <input type="submit" value="Dịch"><br>
+        <h5>Từ cần dịch: <span style="color: blue">${tiengViet}</span></h5>
+        <h5>Kết quả: <span style="color: blue">${result}</span></h5>
+    </fieldset>
+</form>
+</body>
 </html>
